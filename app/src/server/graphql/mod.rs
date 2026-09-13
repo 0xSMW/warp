c082ed9f75546bc7f4770debd248b3a7c3e87980
Stephen <12349+0xSMW@users.noreply.github.com>
2026-09-13 15:58:45 +0700

    test(fork): align server/graphql helpers with local-only targets

--- app/src/server/graphql/mod.rs
+++ app/src/server/graphql/mod.rs
@@ -1,5 +1,7 @@
 pub mod schema;
 
 pub use warp_graphql::client::GraphQLError;
+#[cfg(test)]
+pub use warp_graphql::client::get_request_context;
 #[cfg(any(test, all(feature = "tui", feature = "test-util")))]
-pub use warp_graphql::client::{get_request_context, get_user_facing_error_message};
+pub use warp_graphql::client::get_user_facing_error_message;
